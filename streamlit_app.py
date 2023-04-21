552ad1ea4da46cb6c9bddd6175bf9ddde355174b
mail16 <99182230+mail16@users.noreply.github.com>
2023-04-21 12:56:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,9 @@ try:
         streamlit.error("Please select fruit to grt information")
    else:
     
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   streamlit.dataframe(fruityvice_normalized)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
    streamlit.error()
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
